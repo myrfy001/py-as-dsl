2893bab0e277ca4ca00d08e57105fb66be6b732b
myrfy001 <myrfy001@users.noreply.github.com>
2020-04-10 21:56:00 +0800

    go里的简易调用实现

--- golang/main.go
+++ golang/main.go
@@ -3,25 +3,25 @@ package main
 // #cgo LDFLAGS: -lrustpythoncbinding
 // #cgo LDFLAGS: -L/Users/mmhh/study/RustPythonMini/target/debug
 // void test_main(void);
+// void *rpy_compile_code(void *vm, const char *code);
+// void *rpy_new_scope_with_builtins(void *vm);
+// void rpy_run_code_obj(void *vm, void *code_obj, void *scope);
+// void *rpy_vm_new(void);
 import "C"
 
-import (
-	"fmt"
-	"time"
-)
 
-func test(x int) {
-	for {
-		st := time.Now().UnixNano()
-		C.test_main()
-		fmt.Println(x, time.Now().UnixNano() - st)
-	}
+
+func test() {
+	
+	vm := C.rpy_vm_new()
+	scope := C.rpy_new_scope_with_builtins(vm)
+	code_obj := C.rpy_compile_code(vm, C.CString("print(\"123123\")"))
+	C.rpy_run_code_obj(vm, code_obj, scope)
+	C.rpy_run_code_obj(vm, code_obj, scope)
+	
 }
 
 func main() {
-	for x:=0; x<4; x++ {
-		go test(x)
-	}
-	time.Sleep(time.Second*1000)
+	test()
 
 }
--- golang/readme.md
+++ golang/readme.md
@@ -1,1 +1,6 @@
 https://stackoverflow.com/questions/55282165/dylib-cannot-load-libstd-when-compiled-in-a-workspace
+
+
+source "$HOME/.cargo/env"
+export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+export DYLD_LIBRARY_PATH="$(rustc --print sysroot)/lib:$DYLD_LIBRARY_PATH"
